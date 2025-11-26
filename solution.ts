f0ecd5fdedc85b1f07270ab166a0bd023b649d24
esmotara-codec <esmotara1725@gmail.com>
2025-11-26 10:40:15 +0600

    problem done

--- solution.ts
+++ solution.ts
@@ -32,7 +32,7 @@
 //     }
 
 //       getDetails(): string{
-//         return ` 'Name: ${this.name} , age: ${this.age}' `
+//         return ` 'Name: ${this.name} , Age: ${this.age}' `
 //     }
 // }
 
@@ -73,87 +73,109 @@
 //   { id: 3, name: 'Rumi', email: 'rumi@example.com', isActive: true },
 // ];
 
-interface Book {
-  title: string;
-  author: string;
-  publishedYear: number;
-  isAvailable: boolean;
+// interface Book {
+//   title: string;
+//   author: string;
+//   publishedYear: number;
+//   isAvailable: boolean;
+// }
+
+// function printBookDetails(book : Book): void {
+//     const availabilty = book.isAvailable ? "Yes" : "No" ;
+
+//     // console.log(
+//     //     `Title: ${book.title}, Author: ${book.author}, Published: ${book.publishedYear}, Available: ${availabilty}`
+//     // );
+
+// }
+
+// const myBook: Book = {
+//   title: 'The Great Gatsby',
+//   author: 'F. Scott Fitzgerald',
+//   publishedYear: 1925,
+//   isAvailable: true,
+// };
+
+// printBookDetails(myBook);
+
+// function getUniqueValues(
+//   arr1 : (string | number)[] ,
+//   arr2 : ( string | number)[],
+// ): (string | number)[] {
+//     const result : (string | number)[]  = [];
+
+//     // Add value  from arr1 if not exists
+//     for ( let i = 0 ; i < arr1.length ; i++){
+//       const value = arr1[i];
+//       let exists = false ;
+
+//       for(let j =0 ; j < arr1.length ; j++){
+//        if(result[j] === value){
+//         exists =true;
+//         break;
+//        }
+
+//       }
+
+//       if(!exists){
+//         result.push(value);
+//       }
+
+//     }
+
+//       // Add value from arr2 if not exists
+//      for ( let i = 0 ; i < arr2.length ; i++){
+//       const value = arr2[i];
+//       let exists = false ;
+
+//       for(let j =0 ; j < arr2.length ; j++){
+//        if(result[j] === value){
+//         exists =true;
+//         break;
+//        }
+
+//       }
+
+//       if(!exists){
+//         result.push(value);
+//       }
+
+//     }
+
+//     return result;
+
+// }
+
+// const array1 = [1, 2, 3, 4, 5];
+// const array2 = [3, 4, 5, 6, 7];
+
+interface Product {
+  name: string;
+  price: number;
+  quantity: number;
+  discount?: number;
 }
 
-function printBookDetails(book : Book): void {
-    const availabilty = book.isAvailable ? "Yes" : "No" ;
+function calculateTotalPrice (products : Product[] ){
+     if (products.length === 0) return 0;
 
-    // console.log(
-    //     `Title: ${book.title}, Author: ${book.author}, Published: ${book.publishedYear}, Available: ${availabilty}`
-    // );
+     return products.map(product => {
+      const realPrice = product.price * product.quantity;
+
+      if (product.discount !== undefined){
+        const discountedPrice = (realPrice * product.discount)/100;
+        return realPrice - discountedPrice;
+      }
+
+      return realPrice;
+     } )
+     .reduce((sum , price) => sum + price , 0); 
 
 }
 
-const myBook: Book = {
-  title: 'The Great Gatsby',
-  author: 'F. Scott Fitzgerald',
-  publishedYear: 1925,
-  isAvailable: true,
-};
-
-printBookDetails(myBook);
-
-
-function getUniqueValues(
-  arr1 : (string | number)[] ,
-  arr2 : ( string | number)[],
-): (string | number)[] {
-    const result : (string | number)[]  = [];
-    
-    // Add value  from arr1 if not exists
-    for ( let i = 0 ; i < arr1.length ; i++){
-      const value = arr1[i];
-      let exists = false ;
-
-      for(let j =0 ; j < arr1.length ; j++){
-       if(result[j] === value){
-        exists =true;
-        break;
-       }
-
-      }
-      
-      if(!exists){
-        result.push(value);
-      }
-
-    }
-
-      // Add value  from arr2 if not exists
-     for ( let i = 0 ; i < arr2.length ; i++){
-      const value = arr2[i];
-      let exists = false ;
-
-      for(let j =0 ; j < arr2.length ; j++){
-       if(result[j] === value){
-        exists =true;
-        break;
-       }
-
-      }
-      
-      if(!exists){
-        result.push(value);
-      }
-
-    }
-
-    return result;
-
-}
-
-
-const array1 = [1, 2, 3, 4, 5];
-const array2 = [3, 4, 5, 6, 7];
-console.log(getUniqueValues(array1, array2));
-
-
-
-
-
+const products = [
+  { name: 'Pen', price: 10, quantity: 2 },
+  { name: 'Notebook', price: 25, quantity: 3, discount: 10 },
+  { name: 'Bag', price: 50, quantity: 1, discount: 20 },
+];
 
